c6c9a1c3402b68c714eea5a2a499c5e54b49e765
Aniket <aniketbaranwal8090@gmail.com>
2025-07-22 20:58:21 +0530

    Modifying axiosClient for deployed url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,25 +2,12 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 // Basic API client
+console.log(import.meta.env.VITE_API_BASE_URL)
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_URL || "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
-
-// api.interceptors.request.use(
-//   (config) => {
-    
-//     const token = localStorage.getItem('eduToken');
-    
-//     if (token) {
-//       config.headers = config.headers || {};
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
 
 api.interceptors.response.use(
   (response) => response.data,
